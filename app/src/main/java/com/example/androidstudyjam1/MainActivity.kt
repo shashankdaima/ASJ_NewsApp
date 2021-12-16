4a95136d7e70efc6fc3ed46c2edfc96354a5f857
shashank <shashank10@iiitd.ac.in>
2021-12-17 01:09:50 +0530

    initial commit.

---
+++ app/src/main/java/com/example/androidstudyjam1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidstudyjam1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
